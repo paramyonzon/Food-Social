e3fb6d3e661db6147d7697d2ddce5b73c3e89aef
Param Yonzon <paramyonzon@Params-MacBook-Air.local>
2017-06-30 23:28:42 -0400

    Completeted Setting Up Firebase Integration

---
+++ Food-Social/SignInVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Food-Social
+//
+//  Created by Param Yonzon on 6/30/17.
+//  Copyright © 2017 Param Yonzon. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController { 
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    }
+}
+
