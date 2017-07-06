219e359e2a327b4c23e1185040bf20250f1b460f
Param Yonzon <paramyonzon@Params-MacBook-Air.local>
2017-07-05 20:38:44 -0400

    Sign in ui custom classes

---
+++ Food-Social/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Food-Social
+//
+//  Created by Param Yonzon on 7/5/17.
+//  Copyright © 2017 Param Yonzon. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
+
+
---
+++ Food-Social/RoundBtn.swift
@@ -0,0 +1,24 @@
+//
+//  RoundBtn.swift
+//  Food-Social
+//
+//  Created by Param Yonzon on 7/5/17.
+//  Copyright © 2017 Param Yonzon. All rights reserved.
+//
+
+import UIKit
+
+class RoundBtn: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0 )
+        imageView?.contentMode = .scaleAspectFit
+    }
+
+    
+}
